f3f72a15ae3c86e8cf97dcc61d807e80b4a8bbb6
i330014059 <i330014059@CZ15A008.uic.local>
2016-03-11 11:23:25 +0800

    Week4_Practice_finished

--- src/com/example/week3practice/MainActivity.java
+++ src/com/example/week3practice/MainActivity.java
@@ -2,13 +2,9 @@ package com.example.week3practice;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 	private static int i = 0;
@@ -16,21 +12,57 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final TextView t = (TextView) findViewById(R.id.textView1);
+		final TextView t = (TextView) findViewById(R.id.textNumber);
 		t.setText(Integer.toString(i));
 		
-		Button b = (Button)findViewById(R.id.button1);
-		b.setOnClickListener(new OnClickListener() {
+		final Button btnPlus = (Button)findViewById(R.id.btnPlus);
+		btnPlus.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				final TextView t = (TextView) findViewById(R.id.textView1);
+				final TextView t = (TextView) findViewById(R.id.textNumber);
 				t.setText(Integer.toString(++i));
+			}
+		});
+		final Button btnMinus = (Button)findViewById(R.id.btnMinus);
+		btnMinus.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				final TextView t = (TextView) findViewById(R.id.textNumber);
+				t.setText(Integer.toString(--i));
 				
 			}
 		});
-	}
-	
-	public void OnClickButton(View v){
-		Toast.makeText(this, R.id.button1, Toast.LENGTH_SHORT).show();
+		final Button btnReset = (Button)findViewById(R.id.btnReset);
+		btnReset.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				final TextView t = (TextView) findViewById(R.id.textNumber);
+				i = 0;
+				t.setText(Integer.toString(i));
+			}
+		});
+		final Button btnLogin = (Button)findViewById(R.id.btnLogin);
+		btnLogin.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				EditText editName = (EditText)findViewById(R.id.editName);
+				EditText editPwd = (EditText)findViewById(R.id.editPwd);
+				String name = editName.getText().toString();
+				String pwd = editPwd.getText().toString();
+				String show = name + " - " + pwd;
+				Toast.makeText(v.getContext(), show, Toast.LENGTH_SHORT).show();
+			}
+		});
+		final CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox);
+		checkBox.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Toast.makeText(v.getContext(), isChecked(checkBox), Toast.LENGTH_SHORT).show();
+			}
+			
+			public String isChecked(CheckBox c){
+				if(c.isChecked()){
+					return "Checked!";
+				}else{
+					return "Unchecked!";
+				}
+			}
+		});
 	}
 
 //	@Override
